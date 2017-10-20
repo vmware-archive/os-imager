584c07b5c9dbcace1999cbeb83ed39f784ee4111
Daniel Wallace <danielwallace@gtmanfred.com>
2017-10-20 15:33:10 -0600

    finish arch

--- arch/scripts/install-base.sh
+++ arch/scripts/install-base.sh
@@ -13,7 +13,7 @@ FQDN='arch.saltstack.net'
 KEYMAP='us'
 LANGUAGE='en_US.UTF-8'
 PASSWORD=$(/usr/bin/openssl passwd -crypt 'salt')
-TIMEZONE='UTC'
+TIMEZONE='MST7MDT'
 
 CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
 ROOT_PARTITION="${DISK}1"
@@ -45,7 +45,7 @@ echo "==> Mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
 
 echo '==> Bootstrapping the base installation'
 /usr/bin/pacstrap ${TARGET_DIR} base base-devel
-/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm gptfdisk openssh syslinux
+/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm gptfdisk openssh syslinux ruby
 /usr/bin/arch-chroot ${TARGET_DIR} syslinux-install_update -i -a -m
 /usr/bin/sed -i "s|sda3|${ROOT_PARTITION##/dev/}|" "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
 /usr/bin/sed -i 's/TIMEOUT 50/TIMEOUT 10/' "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
@@ -56,17 +56,20 @@ echo '==> Generating the filesystem table'
 echo '==> Generating the system configuration script'
 /usr/bin/install --mode=0755 /dev/null "${TARGET_DIR}${CONFIG_SCRIPT}"
 
+echo '==> Copy OneContext PKGBUILD to root'
+/usr/bin/mkdir "${TARGET_DIR}/context"
+/usr/bin/install --mode=0644 /PKGBUILD "${TARGET_DIR}/context/PKGBUILD"
+
 cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
 	echo '${FQDN}' > /etc/hostname
-	/usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+	/usr/bin/ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 	echo 'KEYMAP=${KEYMAP}' > /etc/vconsole.conf
 	/usr/bin/sed -i 's/#${LANGUAGE}/${LANGUAGE}/' /etc/locale.gen
 	/usr/bin/locale-gen
-	/usr/bin/mkinitcpio -p linux
 	/usr/bin/usermod --password ${PASSWORD} root
 	# https://wiki.archlinux.org/index.php/Network_Configuration#Device_names
-	/usr/bin/ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
-	/usr/bin/ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
+	/usr/bin/ln -sf /dev/null /etc/udev/rules.d/80-net-setup-link.rules
+	/usr/bin/ln -sf '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
 	/usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
 	/usr/bin/systemctl enable sshd.service
     /usr/bin/useradd --password ${PASSWORD} --comment 'Salt User' --create-home --user-group salt
@@ -74,13 +77,27 @@ cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
 	echo 'salt ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_salt
 	/usr/bin/chmod 0440 /etc/sudoers.d/10_salt
 
+    echo '===> Installing OpenNebula Stuff'
+    echo '===> mkinitcpio-growrootfs'
+    sed -i 's:^PRESETS.*:PRESETS=(default):' /etc/mkinitcpio.d/linux.preset
+    sed -i 's:filesystems:& growfs:' /etc/mkinitcpio.conf
+    curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/mkinitcpio-growrootfs.tar.gz | tar -C / -xzf -
+    cd /mkinitcpio-growrootfs/
+    chown -R salt:salt .
+    sudo -u salt makepkg -si --noconfirm
+
+    echo '===> context'
+    cd /context
+    chown -R salt:salt .
+    sudo -u salt makepkg -sic --noconfirm
+
 	# clean up
 	/usr/bin/pacman -Rcns --noconfirm gptfdisk
 EOF
 
 echo '==> Entering chroot and configuring system'
 /usr/bin/arch-chroot ${TARGET_DIR} ${CONFIG_SCRIPT}
-rm "${TARGET_DIR}${CONFIG_SCRIPT}"
+rm -rf "${TARGET_DIR}${CONFIG_SCRIPT}" "${TARGET_DIR}/context" "${TARGET_DIR}/mkinitcpio-growrootfs/"
 
 echo '==> Installation complete!'
 /usr/bin/sleep 3
