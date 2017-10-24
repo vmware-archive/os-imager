8e9c5e86927155e8c7e753a6a4a7f3b0dbd03d99
Packer Automation <operations@saltstack.com>
2017-10-24 15:26:37 +0000

    Adding ubuntu packer scripts.

---
+++ ubuntu/scripts/vm-context.sh
@@ -0,0 +1,5 @@
+# install open nebula vm context
+wget https://github.com/OpenNebula/addon-context-linux/releases/download/v5.0.3/one-context_5.0.3.deb
+dpkg -i one-context*deb
+apt-get install ruby # only needed for onegate command
+apt-get install -y cloud-utils
