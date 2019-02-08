af0d568aa87715019727cd3e6501ef31ffb394c9
dubb-b <bbenassi@saltstack.com>
2019-02-07 17:14:00 -0700

    Adding more windows versions and RHEL 8Beta

--- AWS/windows/scripts/SetUpWinRM.ps1
+++ AWS/windows/scripts/SetUpWinRM.ps1
@@ -10,6 +10,8 @@ $ErrorActionPreference = "stop"
 
 # Remove HTTP listener
 Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
+Set-Item WSMan:\localhost\MaxTimeoutms 1800000
+Set-Item WSMan:\localhost\Service\Auth\Basic $true
 
 $Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
