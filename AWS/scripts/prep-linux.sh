ce033a552f05346bc79f14c8040ea57f961b696a
Pedro Algarvio <pedro@algarvio.me>
2019-02-28 16:05:05 +0000

    Consolidate prep work for both windows and linux

--- AWS/scripts/prep-linux.sh
+++ AWS/scripts/prep-linux.sh
@@ -3,14 +3,10 @@
 dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
 # Clone State Tree
-. ${dir}/clone-salt-jenkins.sh
+. ${dir}/prep-minion-conf.sh
+. ${dir}/prep-states.sh
+. ${dir}/prep-pillar.sh
 
-if [ ! -d .tmp/pillar ]; then
-    mkdir -p .tmp/pillar
-fi
-
-echo "Building Pillar Data for Python Version: ${PY_VERSION}"
-printf "base:\n  '*':\n    - base\n" > .tmp/pillar/top.sls
-printf "py$PY_VERSION: true\n" > .tmp/pillar/base.sls
-printf "packer_build: true\n" >> .tmp/pillar/base.sls
-printf "create_testing_dir: false\n" >> .tmp/pillar/base.sls
+echo "Copying gitpython.sls to the temp states directory"
+cp ${dir}/gitpython.sls .tmp/states/
+ls -lah .tmp/states
---
+++ AWS/scripts/prep-pillar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building Pillar Data for Python Version: ${PY_VERSION}"
+if [ -d .tmp/pillar ]; then
+    rm -rf .tmp/pillar
+fi
+mkdir -p .tmp/pillar
+printf "base:\n  '*':\n    - base\n" > .tmp/pillar/top.sls
+printf "py$PY_VERSION: true\n" > .tmp/pillar/base.sls
+printf "packer_build: true\n" >> .tmp/pillar/base.sls
+printf "create_testing_dir: false\n" >> .tmp/pillar/base.sls
