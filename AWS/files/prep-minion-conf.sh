fe5b504ec07af3552f8e83fe2c15d8423e3c543f
Pedro Algarvio <pedro@algarvio.me>
2019-03-01 15:48:10 +0000

    Hardcode the minion ID

--- AWS/files/prep-minion-conf.sh
+++ AWS/files/prep-minion-conf.sh
@@ -5,7 +5,9 @@ if [ -d .tmp/conf ]; then
 fi
 mkdir -p .tmp/conf
 
-printf "fileserver_backend:\n  - roots\n  - gitfs\n" > .tmp/conf/minion
+printf "id: packer-local-minion\n" > .tmp/conf/minion
+printf "minion_id_caching: True\n" >> .tmp/conf/minion
+printf "fileserver_backend:\n  - roots\n  - gitfs\n" >> .tmp/conf/minion
 printf "gitfs_base: ${SALT_BRANCH}\n" >> .tmp/conf/minion
 #printf "gitfs_remotes:\n  - https://github.com/saltstack/salt-jenkins.git\n" >> .tmp/conf/minion
 printf "gitfs_remotes:\n  - https://github.com/s0undt3ch/salt-jenkins.git\n" >> .tmp/conf/minion
