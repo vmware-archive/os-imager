c605be3a880f7d83b41ae81a6a6ab3e66de1a17d
Pedro Algarvio <pedro@algarvio.me>
2019-03-04 11:42:41 +0000

    Type and echo download URL

--- AWS/files/prep-windows.sh
+++ AWS/files/prep-windows.sh
@@ -11,11 +11,13 @@ if [ "$PY_VERSION" -eq 2 ]; then
 elif [ "$PY_VERSION" -eq 3 ]; then
     PYTHON_DIR="C:\\Python35"
 else
-    echo "Don't know how to hanle PY_VERSION $PY_VERSION"
+    echo "Don't know how to handle PY_VERSION $PY_VERSION"
     exit 1
 fi
 printf "python_install_dir: ${PYTHON_DIR}\n" > .tmp/pillar/windows.sls
 printf "virtualenv_path: ${PYTHON_DIR}\\Scripts\\pip.exe\n" >> .tmp/pillar/windows.sls
 
 # Download the powershell script that installs Git on windows
-curl -L https://github.com/tomlarse/Install-Git/raw/master/Install-Git/Install-Git.ps1 --output .tmp/scripts/Install-Git.ps1
+URL="https://github.com/tomlarse/Install-Git/raw/master/Install-Git/Install-Git.ps1"
+echo "Downloading ${URL}"
+curl -L $URL --output .tmp/scripts/Install-Git.ps1
