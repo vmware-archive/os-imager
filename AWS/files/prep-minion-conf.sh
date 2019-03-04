70fbc5e0df1ecfaa46500c7dd6d2d76313917eb1
Pedro Algarvio <pedro@algarvio.me>
2019-03-04 11:42:22 +0000

    Disable old windows repo

--- AWS/files/prep-minion-conf.sh
+++ AWS/files/prep-minion-conf.sh
@@ -11,3 +11,5 @@ printf "fileserver_backend:\n  - roots\n  - gitfs\n" >> .tmp/conf/minion
 printf "gitfs_base: ${SALT_BRANCH}\n" >> .tmp/conf/minion
 #printf "gitfs_remotes:\n  - https://github.com/saltstack/salt-jenkins.git\n" >> .tmp/conf/minion
 printf "gitfs_remotes:\n  - https://github.com/s0undt3ch/salt-jenkins.git\n" >> .tmp/conf/minion
+# Disable the old windows repo
+printf "winrepo_remotes: []\n" >> .tmp/conf/minion
