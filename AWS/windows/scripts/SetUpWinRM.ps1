dd0cf8d6ef9da83588e806f60adbbba0bb324d89
Pedro Algarvio <pedro@algarvio.me>
2019-06-07 10:30:29 +0100

    Revert "Rework how pagefile, winrm and powershel memory settings are configured"
    This reverts commit eeb089621c9daa891dfc7882c564b65134bfb09d.

--- AWS/windows/scripts/SetUpWinRM.ps1
+++ AWS/windows/scripts/SetUpWinRM.ps1
@@ -22,6 +22,9 @@ write-host "(host) setting up WinRM"
 
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="7500"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxProcessesPerShell="100"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxConcurrentCommandsPerShell="7500"}'
 cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
@@ -30,22 +33,9 @@ cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
 cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
 cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
 cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
-# WinRS Tweaks
-Set-Item WSMan:\localhost\Shell\MaxConcurrentUsers 100
-set-item WSMan:\localhost\Shell\MaxProcessesPerShell 10000
-set-item WSMan:\localhost\Shell\MaxMemoryPerShellMB 7500
-set-item WSMan:\localhost\Shell\MaxShellsPerUser 1000
-# PowerShell Plugin Tweaks
-Set-Item WSMan:\localhost\Plugin\Microsoft.PowerShell\Quotas\MaxConcurrentUsers 100
-Set-Item WSMan:\localhost\Plugin\Microsoft.PowerShell\Quotas\MaxProcessesPerShell 10000
-Set-Item WSMan:\localhost\Plugin\Microsoft.PowerShell\Quotas\MaxShells 1000
-Set-Item WSMan:\localhost\Plugin\Microsoft.PowerShell\Quotas\MaxShellsPerUser 1000
-# Auto Startup & Restart
 cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
 
-# Pagefile
-cmd.exe /c --% reg add "hklm\system\currentcontrolset\control\session manager\memory management" /v pagingfiles /t reg_multi_sz /d "D:\pagefile.sys 8912 8912" /f
 </powershell>
 <persist>true</persist>
