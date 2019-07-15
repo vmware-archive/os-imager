cfe0b97550f379d438c58e09279aa6d3ff378b68
Felippe Burk <fburk@saltstack.com>
2019-07-15 11:02:15 -0600

    using main salt-jenkins repo

--- AWS/files/prep-minion-conf.sh
+++ AWS/files/prep-minion-conf.sh
@@ -9,7 +9,6 @@ printf "id: packer-local-minion\n" > .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/min
 printf "minion_id_caching: True\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
 printf "fileserver_backend:\n  - roots\n  - gitfs\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
 printf "gitfs_base: ${SALT_BRANCH}\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
-#printf "gitfs_remotes:\n  - https://github.com/saltstack/salt-jenkins.git\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
-printf "gitfs_remotes:\n  - https://github.com/s0undt3ch/salt-jenkins.git\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
+printf "gitfs_remotes:\n  - https://github.com/saltstack/salt-jenkins.git\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
 # Disable the old windows repo
 #printf "winrepo_remotes: []\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
