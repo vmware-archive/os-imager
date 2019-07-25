80ccff47aca88cc21e85a0a901dc32e6192ad09b
Bryce Larson <blarson@saltstack.com>
2019-07-25 15:07:17 -0600

    remove gitfs

--- AWS/files/prep-minion-conf.sh
+++ AWS/files/prep-minion-conf.sh
@@ -7,8 +7,8 @@ mkdir -p .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf
 
 printf "id: packer-local-minion\n" > .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
 printf "minion_id_caching: True\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
-printf "fileserver_backend:\n  - roots\n  - gitfs\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
-printf "gitfs_base: ${SALT_BRANCH}\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
-printf "gitfs_remotes:\n  - https://github.com/saltstack/salt-jenkins.git\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
+#printf "fileserver_backend:\n  - roots\n  - gitfs\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
+#printf "gitfs_base: ${SALT_BRANCH}\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
+#printf "gitfs_remotes:\n  - https://github.com/saltstack/salt-jenkins.git\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
 # Disable the old windows repo
 #printf "winrepo_remotes: []\n" >> .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/conf/minion
--- AWS/files/prep-states.sh
+++ AWS/files/prep-states.sh
@@ -5,5 +5,7 @@ if [ -d .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states ]; then
     rm -rf .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states
 fi
 mkdir -p .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states
+git clone https://github.com/saltstack/salt-jenkins.git -b ${SALT_BRANCH} .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states
+rm -rf .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states/.git
 
 printf "noop:\n  test.succeed_without_changes" > .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states/empty.sls
