e9ea12362ff69b7b0dc95b91e2b6fc18fd968999
Bryce Larson <blarson@saltstack.com>
2019-08-08 11:57:10 -0600

    change git clone message

--- AWS/files/prep-states.sh
+++ AWS/files/prep-states.sh
@@ -7,8 +7,7 @@ fi
 mkdir -p .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states
 git clone https://github.com/saltstack/salt-jenkins.git -b ${SALT_BRANCH} .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states
 echo -n "Current git HEAD: "
-(cd .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states ; git rev-parse HEAD)
-echo
+(cd .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states ; git rev-parse HEAD ; echo ; git log -1 --pretty=%B ; echo)
 rm -rf .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states/.git
 
 printf "noop:\n  test.succeed_without_changes" > .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states/empty.sls
