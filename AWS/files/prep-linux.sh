dbe160be00ec9842229a3230ad31e6ace045b750
Pedro Algarvio <pedro@algarvio.me>
2019-09-17 21:27:31 +0100

    Run a noop state just to get salt installed

--- AWS/files/prep-linux.sh
+++ AWS/files/prep-linux.sh
@@ -7,6 +7,6 @@ dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 . ${dir}/prep-states.sh
 . ${dir}/prep-pillar.sh
 
-echo "Copying gitpython.sls to the temp states directory"
-cp ${dir}/gitpython.sls .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states/
+echo "Copying noop.sls to the temp states directory"
+cp ${dir}/noop.sls .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states/
 ls -lah .tmp/${DISTRO_SLUG}/${SALT_BRANCH}/states
